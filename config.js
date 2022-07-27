2b2e8bc4ef6033ff779386210a4b652231bd2320
DTTL <102647478+NTH077@users.noreply.github.com>
2022-07-27 08:36:07 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,17 +1,19 @@
 const CONFIG = {
-    titleWeb: "Gửi em....<3",
-    introTitle: 'Nguyễn Thị Hòa',
-    introDesc: 'Đêm nay trăng sáng, anh ôm cây đàn. Lan man câu hát đưa bước anh theo chân nàng. Em cho anh thấy Cảm giác của mối tình đầu',
+    titleWeb: "This is for you",
+    introTitle: 'Đào Thị Thùy Linh',
+    introDesc: `Đêm nay trăng sáng, anh ôm cây đàn. 
+        Lan man câu hát đưa bước anh theo chân nàng.
+        Em cho anh thấy Cảm giác của mối tình đầu`,
     btnIntro: '^^HiHi^^',
-    title: 'Lâu chưa biết nhớ, cũng chẳng thấy mơ. Ngày đẹp trời nơi này bỗng nhiên em xuất hiện. Làm trái tim anh rung động 🥰',
+    title: 'Lâu chưa biết nhớ, cũng chẳng thấy mơ. Ngày đẹp trời nơi này bỗng nhiên em xuất hiện. Làm trái tim anh rung động ',
     desc: 'Nếu như bây giờ ngỏ lời yêu?? Biết đâu ta trở thành một đôi ',
-    btnYes: 'Thích lắm <33',
+    btnYes: 'Thích lắm <3<3',
     btnNo: 'Không nha :3',
     question: 'Anh tấn công em như vậy tại sao em còn chưa bỏ lớp chắn con tym ra cho anh vào hử :3',
-    btnReply: 'Gửi cho anh <3',
-    reply: 'Tại em hơi ngại thui. Nhưng mà anh dễ thương như thế này chắc là phải yêu hoi',
-    mess: 'Anh biết mà 🥰. Yêu em nhắm 😘😘',
-    messDesc: 'Tối nay 9h, mình call video nhận ny nha.',
-    btnAccept: 'DẠ anh iu  <3',
+    btnReply: 'SEND ME ❤',
+    reply: 'Tại em hơi ngại thui. Nhưng mà anh đáng yêu như thế này chắc là phải yêu hoi',
+    mess: 'Thầy biết mà 😛, Lớp diiuuuuu <<<<<<333333',
+    messDesc: 'Tối nay 10h, thầy mở lớp dậy lớp diu nhớ nt confirm ở đây nha.',
+    btnAccept: 'Dạaaaaa thầy ❤',
     messLink: 'https://www.facebook.com/kinhkong.s2/' //link mess của các bạn. VD: https://www.facebook.com/messages/t/100014188333536
 }
